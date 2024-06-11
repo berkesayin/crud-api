c046cb3b83a3a52215251688f6cb04a2206a366e
Berke Sayın <sayinberke34@gmail.com>
2024-06-11 15:49:22 +0300

    Add User repository interface

---
+++ src/main/java/dev/berkesayin/crud_api/user/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.berkesayin.crud_api.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
